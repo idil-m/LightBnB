6677c38340cae0b16e16cc07890e3090c13ca335
Idil Mahamed <mahamed.idil@gmail.com>
2023-11-23 02:28:15 -0500

    updated getAllProperties function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -119,21 +119,56 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const query = {
-    text: `SELECT * FROM properties LIMIT $1`,
-    values: [limit]
-  };
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    LEFT JOIN property_reviews ON properties.id = property_id
+  `;
 
-  return pool
-    .query(query)
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.error("Error executing query:", err);
-      throw err; 
-    });
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} owner_id = $${queryParams.length} `;
+  }
+
+ 
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100); 
+    queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100); 
+    queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night <= $${queryParams.length} `;
+  }
+
+  
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `${queryParams.length === 1 ? 'HAVING' : 'AND'} avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  
+  queryParams.push(limit);
+  queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+
+  
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
