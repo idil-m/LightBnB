9b25e7b57fd05197c24900937d5db9c7b0b7807c
Idil Mahamed <mahamed.idil@gmail.com>
2023-11-23 02:00:10 -0500

    updated getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -87,7 +87,27 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = {
+    text: `SELECT *, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2
+    `,
+    values: [guest_id, limit]
+  };
+  return pool
+    .query(query)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error("Error executing query:", err);
+      throw err; 
+    });
 };
 
 /// Properties
